f3f4d34e2224ebec48529ee0feaf3330ef92c0a4
Carlos Lins <carloslins@BRSPPM-C02GKGPWMD6M.local>
2022-10-27 21:49:05 -0300

    final version

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from time import sleep
-import requests
-import os
 import csv
+import os
+from time import sleep
 
+import requests
 from bs4 import BeautifulSoup as bs
 
 
 def search_jobs(job, location="Brazil"):
 
-    req = requests.get(f"https://www.linkedin.com/jobs/search?keywords={job}&location={location}&geoId=106057199&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0")
+    req = requests.get(f"https://www.linkedin.com/jobs/search?keywords={job}&location={location}&locationId=&geoId=106057199&f_TPR=r86400&position=1&pageNum=0")
     req.raise_for_status()
     page_soup = bs(req.text, 'html.parser')
 
@@ -21,7 +21,7 @@ def search_jobs(job, location="Brazil"):
     # pages = [25]
 
     # for i in pages:
-    #     req = requests.get(f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={job}&location={location}&geoId=106057199&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start={i}")
+    #     req = requests.get(f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={job}&location={location}&locationId=&geoId=106057199&f_TPR=r86400&start={i}")
     #     req.raise_for_status()
     #     page_soup2 = bs(req.text, 'html.parser')
     #     for links in res_card.findAll('a', {'class': 'base-card__full-link'}):
@@ -33,7 +33,8 @@ def get_jobs_info(links, place):
     csv_filename = f'jobs_in_{place}.csv'
     with open(os.path.join("jobs", csv_filename), 'w', encoding='utf-8') as f:
         headers = ['Source', 'Organization', 'Job Title', 'Location', 'Posted', 'Seniority Level',
-                    'Employment Type', 'Job Function', 'Industries']
+                    'Employment Type', 'Job Function', 'Description']
+        
         write = csv.writer(f, dialect='excel')
         write.writerow(headers)
 
@@ -44,7 +45,8 @@ def get_jobs_info(links, place):
 
             # Parse HTML
             job_soup = bs(page_req.text, 'html.parser')
-            my_data = [job_link]
+            my_data = ["Empty"] * 9
+            my_data[0] = job_link
 
             # Topcard scraping                
             for content in job_soup.findAll('div', {'class': 'top-card-layout__card'})[0:]:
@@ -54,17 +56,17 @@ def get_jobs_info(links, place):
 
                 if orgs['Default-Org'] == []:
                     org = orgs['Flavor-Org'][0]
-                    my_data.append(org)
+                    my_data[1] = org
                 else:
                     for org in orgs['Default-Org']:
-                        my_data.append(org)
+                        my_data[1] = org
 
                 # Scraping Job Title
                 for title in content.findAll('h1', {'class': 'topcard__title'})[0:]:
-                    my_data.append(title.text.replace(',', '.'))
+                    my_data[2] = title.text.replace(',', '.')
 
                 for location in content.findAll('span', {'class': 'topcard__flavor topcard__flavor--bullet'})[0:]:
-                    my_data.append(location.text.replace(',', '.'))
+                    my_data[3] = location.text.replace(',', '.')
 
                 # Scraping Job Time Posted
                 posts = {'Old': [posted.text for posted in content.findAll('span', {'class': 'topcard__flavor--metadata posted-time-ago__text'})],
@@ -72,16 +74,20 @@ def get_jobs_info(links, place):
 
                 if posts['New'] == []:
                     for text in posts['Old']:
-                        my_data.append(text)
+                        my_data[4] = text
                 else:
                     for text in posts['New']:
-                        my_data.append(text)
-
+                        my_data[4] = text
 
             # Criteria scraping
-            for criteria in job_soup.findAll('span', {'class': 'job-criteria__text job-criteria__text--criteria'})[:4]:
-                my_data.append(criteria.text)
-            print(my_data)
+            for  i, criteria in enumerate(job_soup.findAll('span', {'class': 'description__job-criteria-text description__job-criteria-text--criteria'})[:5]):
+                my_data[i + 5] = criteria.text
+            
+            description_text = ""
+            for description in job_soup.findAll('div', {'class': 'show-more-less-html__markup show-more-less-html__markup--clamp-after-5'})[0:]:
+                for desc in description:
+                    description_text = description_text + str(desc.text)
+            my_data[8] = description_text
             write.writerows([my_data])
 
 search_jobs("desenvolvedor")
